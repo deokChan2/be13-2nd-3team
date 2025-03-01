34f4c4e3c02ec04bb39a82c7571fa7cde01c419c
DESKTOP-UUHNBLN\Playdata <ejrcks1690@naver.com>
2025-03-01 09:41:31 +0900

    03-01 수정

--- yyGang/src/main/java/com/beyond3/yyGang/cart/dto/AddCartOptionRequestDto.java
+++ yyGang/src/main/java/com/beyond3/yyGang/cart/dto/AddCartOptionRequestDto.java
@@ -1,5 +1,6 @@
 package com.beyond3.yyGang.cart.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -11,10 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AddCartOptionRequestDto {
 
-//    @NotNull(message = "상품이 제대로 확인되지 않았습니다.")
+    @NotNull(message = "상품이 제대로 확인되지 않았습니다.")
     private Long nSupplementId;
 
-    // @NotNull(message = "개수를 정확히 입력하세요.")
+    @Min(value = 1, message = "수량은 최소 1개 이상이어야 합니다.")
     private int quantity;
 
 }
